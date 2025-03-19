112f15854cf2611454daf02d9a0133f00dad8d4f
Aayushrohilla <aayushrohilla3@gmail.com>
2025-03-19 14:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+it('testing pseudo class "before" content and color', () => {
+  cy.get('#container')
+    .should('have.css', 'color', 'rgb(255, 0, 0)') // Check if color is red
+    .and('have.css', 'content', '"Before element"'); // Ensure content is as expected
+});
